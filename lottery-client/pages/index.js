d9618d7379cd2bdb480a412fc1d5d3a7b6087c4f
Jan Tuđan <jantudjan99@gmail.com>
2022-09-17 01:01:31 +0200

    Added client

---
+++ lottery-client/pages/index.js
@@ -0,0 +1,164 @@
+import { useState, useEffect } from 'react'
+import Head from 'next/head'
+import Web3 from 'web3'
+import lotteryContract from '../blockchain/lottery'
+import styles from '../styles/Home.module.css'
+import 'bulma/css/bulma.css'
+
+export default function Home() {
+  const [error, setError] = useState('')
+  const [web3, setWeb3] = useState()
+  const [address, setAddress] = useState()
+  const [lcContract, setLcContract] = useState()
+  const [lotteryPot, setLotteryPot] = useState()
+  const [lotteryPlayers, setPlayers] = useState([])
+  
+  const [successMsg, setSuccessMsg] = useState('')
+
+  useEffect(() => {
+    updateState()
+  }, [lcContract])
+
+  const updateState = () => {
+    if (lcContract) getPot()
+    if (lcContract) getPlayers()
+  }
+
+  const getPot = async () => {
+    const pot = await lcContract.methods.getBalance().call()
+    setLotteryPot(web3.utils.fromWei(pot, 'ether'))
+  }
+
+  const getPlayers = async () => {
+    const players = await lcContract.methods.getPlayers().call()
+    setPlayers(players)
+  }
+
+  const enterLotteryHandler = async () => {
+    setError('')
+    setSuccessMsg('')
+    try {
+      /* Postavljamo vrijednost jednog uloga na 0.005 Ethera */
+      await lcContract.methods.enter().send({
+        from: address,
+        value: '5000000000000000',
+        gas: 300000,
+        gasPrice: null
+      })
+      updateState()
+    } catch(err) {
+      setError(err.message)
+    }
+  }
+
+
+  const connectWalletHandler = async () => {
+    setError('')
+    setSuccessMsg('')
+    /* Provjera instalacije Metamaska */
+    if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
+      try {
+        /* Traži se konekcija s E-walletom */
+        await window.ethereum.request({ method: "eth_requestAccounts"})
+        const web3 = new Web3(window.ethereum)
+        /* web3 instanca */
+        setWeb3(web3)
+        /* Dohvati listu računa */
+        const accounts = await web3.eth.getAccounts()
+        setAddress(accounts[0])
+
+        /* Lokalna kopija ugovora */
+        const lc = lotteryContract(web3)
+        setLcContract(lc)
+
+        window.ethereum.on('accountsChanged', async () => {
+          const accounts = await web3.eth.getAccounts()
+          console.log(accounts[0])
+          setAddress(accounts[0])
+        })
+      } catch(err) {
+        setError(err.message)
+      }
+    } else {
+      /* MetaMask nije installiran */
+      console.log("Please install MetaMask")
+    }
+  }
+
+
+  return (
+    <div>
+      <Head>
+        <title>Lottery crypto application</title>
+        <meta name="description" content="An Ethereum Lottery dApp" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+      <div className={styles.imgbg}>
+        <img src=""></img>
+        <nav className="navbar mt-4 mb-4">
+          <div className="container">
+            <div className="navbar-brand">
+              <h1>Lottery with Ethers (Rinkeby test network)</h1>
+            </div>
+            <div className="navbar-end">
+              <button onClick={connectWalletHandler} className="button is-link">Connect your Wallet</button>
+            </div>
+          </div>
+        </nav>
+        <div className="container">
+          <section className="mt-5">
+            <div className="columns">
+              <div className="column is-two-thirds">
+                <section className="mt-5">
+                  <p>Join the lottery (0.005 Ether cost)</p>
+                  <button onClick={enterLotteryHandler} className="button is-link is-large is-light mt-3">Play now</button>
+                </section>
+                <section>
+                  <div className="container has-text-danger mt-6">
+                    <p>{error}</p>
+                  </div>
+                </section>
+                <section>
+                  <div className="container has-text-success mt-6">
+                    <p>{successMsg}</p>
+                  </div>
+                </section>
+              </div>
+              <div className={`${styles.lotteryinfo} column is-one-third`}>
+                <section className="mt-5">
+                  <div className="card">
+                    <div className="card-content">
+                      <div className="content">
+                        <h2> Players ({lotteryPlayers.length})</h2>
+                        <ul className="ml-0">
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </section>
+                <section className="mt-5">
+                  <div className="card">
+                    <div className="card-content">
+                      <div className="content">
+                        <h2>Pot</h2>
+                        <p>Ether</p>
+                      </div>
+                    </div>
+                  </div>
+                </section>
+              </div>
+            </div>
+          </section>
+        </div>
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <p>&copy; 2022 Block Explorer</p>
+      </footer>
+    
+    </div>
+  )
+}
